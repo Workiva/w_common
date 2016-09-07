eb8c24a6446c585261b1462968f7cbab307c5b5d
George Lesica <george.lesica@workiva.com>
2016-09-06 21:25:16 -0600

    Make API closer to w_module.

--- lib/src/disposable.dart
+++ lib/src/disposable.dart
@@ -1,24 +1,31 @@
 import 'dart:async';
 
 abstract class Disposable {
+  Completer<Null> _willDispose = new Completer<Null>();
   Completer<Null> _didDispose = new Completer<Null>();
+
   List<StreamSubscription> _subscriptions = [];
   List<Disposable> _disposables = [];
 
-  /// A [Future] that will complete when this object is disposed.
+  /// A [Future] that will complete when this object is about to be disposed.
+  Future<Null> get willDispose => _willDispose.future;
+
+  /// A [Future] that will complete when this object has been disposed.
   Future<Null> get didDispose => _didDispose.future;
 
   /// Whether this object has been disposed.
-  bool get isDisposed => _didDispose.isCompleted;
+  bool get wasDisposed => _didDispose.isCompleted;
 
   /// Dispose of the object, cleaning up to prevent memory leaks.
   Future<Null> dispose() {
-    if (isDisposed) {
+    if (wasDisposed) {
       return new Future(() {});
     }
 
-    _subscriptions.forEach((sub) => sub.cancel);
+    _willDispose.complete();
+
     _disposables.forEach((disposable) => disposable.dispose());
+    _subscriptions.forEach((sub) => sub.cancel());
 
     return onDispose().then((_) {
       _didDispose.complete();
