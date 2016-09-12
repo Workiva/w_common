18c6a8afce579e823510154fcf2e9d42728cac8d
George Lesica <george.lesica@workiva.com>
2016-09-12 13:01:53 -0600

    REF-1236 Improve tests and handle zero-listener streams.

--- test/unit/disposable_test.dart
+++ test/unit/disposable_test.dart
@@ -1,10 +1,9 @@
 import 'dart:async';
 
 import 'package:test/test.dart';
-
 import 'package:w_common/w_common.dart';
 
-typedef void StreamCallback(dynamic _);
+import 'typedefs.dart';
 
 class DisposableThing extends Disposable {
   bool wasOnDisposeCalled = false;
@@ -43,8 +42,30 @@ void main() {
     });
 
     group('manageStreamController', () {
-      test('should close stream when parent is disposed', () async {
+      test('should close a broadcast stream when parent is disposed', () async {
         var controller = new StreamController.broadcast();
+        thing.manageStreamController(controller);
+        expect(controller.isClosed, isFalse);
+        await thing.dispose();
+        expect(controller.isClosed, isTrue);
+      });
+
+      test('should close a single-subscription stream when parent is disposed',
+          () async {
+        var controller = new StreamController();
+        var subscription = controller.stream
+            .listen(expectAsync(([_]) {}, count: 0) as StreamListener);
+        subscription.onDone(expectAsync(([_]) {}, count: 1) as StreamListener);
+        thing.manageStreamController(controller);
+        expect(controller.isClosed, isFalse);
+        await thing.dispose();
+        expect(controller.isClosed, isTrue);
+      });
+
+      test(
+          'should close a single-subscription stream with no listener'
+          'when parent is disposed', () async {
+        var controller = new StreamController();
         thing.manageStreamController(controller);
         expect(controller.isClosed, isFalse);
         await thing.dispose();
@@ -55,8 +76,9 @@ void main() {
     group('manageStreamSubscription', () {
       test('should cancel subscription when parent is disposed', () async {
         var controller = new StreamController();
+        controller.onCancel = expectAsync(([_]) {}, count: 1);
         var subscription = controller.stream
-            .listen(expectAsync((_) {}, count: 0) as StreamCallback);
+            .listen(expectAsync((_) {}, count: 0) as StreamListener);
         thing.manageStreamSubscription(subscription);
         await thing.dispose();
         controller.add(null);
---
+++ test/unit/typedefs.dart
@@ -0,0 +1,1 @@
+typedef void StreamListener([dynamic payload]);
