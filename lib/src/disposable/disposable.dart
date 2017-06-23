d1a538f15809f5b292af7461a497e055cfbdebbd
Smai Fullerton <smai.fullerton@workiva.com>
2017-06-23 15:41:57 -0600

    RAP-2067 Add doc comments and remove redundant null values from optional parameters.

--- lib/src/disposable/disposable.dart
+++ lib/src/disposable/disposable.dart
@@ -428,22 +428,40 @@ class Disposable implements _Disposable, DisposableManagerV3 {
     return null;
   }
 
+  /// Adds an event listener to the document object and removes the event
+  /// listener upon disposal.
+  ///
+  /// If using this method, you cannot manually use the `removeEventListener`
+  /// method on the document singleton to remove the listener. At this point
+  /// the only way to remove the listener is to use the [dispose] method.
   void subscribeToDocumentEvent(String event, EventListener callback,
-      {bool useCapture: null, EventTarget documentObject: null}) {
+      {bool useCapture, EventTarget documentObject}) {
     if (documentObject == null) {
       documentObject = document;
     }
     _subscribeToEvent(documentObject, event, callback, useCapture);
   }
 
+  /// Adds an event listener to the element object and removes the event
+  /// listener upon disposal.
+  ///
+  /// If using this method, you cannot manually use the `removeEventListener`
+  /// method on the element to remove the listener. At this point the only way
+  /// to remove the listener is to use the [dispose] method.
   void subscribeToDomElementEvent(
       Element element, String event, EventListener callback,
-      {bool useCapture: null}) {
+      {bool useCapture}) {
     _subscribeToEvent(element, event, callback, useCapture);
   }
 
+  /// Adds an event listener to the window object and removes the event
+  /// listener upon disposal.
+  ///
+  /// If using this method, you cannot manually use the `removeEventListener`
+  /// method on the window singleton to remove the listener. At this point
+  /// the only way to remove the listener is to use the [dispose] method.
   void subscribeToWindowEvent(String event, EventListener callback,
-      {bool useCapture: null, EventTarget windowObject: null}) {
+      {bool useCapture, EventTarget windowObject}) {
     if (windowObject == null) {
       windowObject = window;
     }
