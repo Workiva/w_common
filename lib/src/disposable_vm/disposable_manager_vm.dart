e7c402c5909b37d2acae57615bcc7c5658a80d3f
Smai Fullerton <smai.fullerton@workiva.com>
2017-06-26 14:17:32 -0600

    RAP-2111 [WIP] Creating VM entry point.

---
+++ lib/src/disposable_vm/disposable_manager_vm.dart
@@ -0,0 +1,121 @@
+// Copyright 2016 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:async';
+
+import 'package:w_common/disposable_vm.dart';
+
+/// Managers for disposable members.
+///
+/// This interface allows consumers to exercise more control over how
+/// disposal is implemented for their classes.
+///
+/// Use DisposableManagerV2 instead.
+@deprecated
+abstract class DisposableManager {
+  /// Automatically dispose another object when this object is disposed.
+  ///
+  /// The parameter may not be `null`.
+  void manageDisposable(Disposable disposable);
+
+  /// Automatically handle arbitrary disposals using a callback.
+  ///
+  /// The parameter may not be `null`.
+  void manageDisposer(Disposer disposer);
+
+  /// Automatically cancel a stream controller when this object is disposed.
+  ///
+  /// The parameter may not be `null`.
+  void manageStreamController(StreamController controller);
+
+  /// Automatically cancel a stream subscription when this object is disposed.
+  ///
+  /// This method should not be used for subscriptions that will be canceled
+  /// manually by the consumer because we have no way of knowing when a
+  /// subscription is canceled, so we will hold on to the reference until the
+  /// parent object is disposed.
+  ///
+  /// The parameter may not be `null`.
+  void manageStreamSubscription(StreamSubscription subscription);
+}
+
+/// Managers for disposable members.
+///
+/// This interface allows consumers to exercise more control over how
+/// disposal is implemented for their classes.
+///
+/// When new management methods are to be added, they should be added
+/// here first, then implemented in [Disposable].
+@deprecated
+abstract class DisposableManagerV2 implements DisposableManager {
+  /// Creates a [Timer] instance that will be cancelled if active
+  /// upon disposal.
+  Timer getManagedTimer(Duration duration, void callback());
+
+  /// Creates a periodic [Timer] that will be cancelled if active
+  /// upon disposal.
+  Timer getManagedPeriodicTimer(Duration duration, void callback(Timer timer));
+}
+
+abstract class DisposableManagerV3 implements DisposableManagerV2 {
+  /// Add [future] to a list of futures that will be awaited before the
+  /// object is disposed.
+  ///
+  /// For example, a long-running network request might use
+  /// a [Disposable] instance when it returns. If we started to dispose
+  /// while the request was pending, upon returning the request's callback
+  /// would throw. We can avoid this by waiting on the request's future.
+  ///
+  ///      class MyApi extends Object with Disposable {
+  ///        MyHelper helper;
+  ///
+  ///        MyApi() {
+  ///          helper = manageDisposable(new MyHelper());
+  ///        }
+  ///
+  ///        Future makeRequest(String message) {
+  ///          return waitBeforeDispose(
+  ///              helper.sendRequest(onSuccess: (response) {
+  ///            // If the `MyApi` instance was disposed while the request
+  ///            // was pending, this would normally result in an exception
+  ///            // being thrown. But instead, the dispose process will wait
+  ///            // for the request to complete before disposing of `helper'.
+  ///            helper.handleResponse(message, response);
+  ///          }))
+  ///        }
+  ///      }
+  Future<T> awaitBeforeDispose<T>(Future<T> future);
+
+  /// Creates a [Future] that will complete, with the value
+  /// returned by [callback], after the given amount of time has elapsed.
+  ///
+  /// If the object is disposed before the time has elapsed the future
+  /// will complete with an [ObjectDisposedException] error.
+  Future<T> getManagedDelayedFuture<T>(Duration duration, T callback());
+
+  /// Ensure that a completer is completed when the object is disposed.
+  ///
+  /// If the completer has not been completed by the time the object
+  /// is disposed, it will be completed with an [ObjectDisposedException]
+  /// error.
+  Completer<T> manageCompleter<T>(Completer<T> completer);
+}
+
+/// Exception thrown when an operation cannot be completed because the
+/// disposable object upon which it depended has been disposed.
+///
+/// For example, if a managed delayed future hasn't completed by the time
+/// the object managing it is disposed, the future will complete with
+/// an instance of this exception.
+class ObjectDisposedException implements Exception {}
---
+++ lib/src/disposable_vm/disposable_vm.dart
@@ -0,0 +1,482 @@
+// Copyright 2016 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:async';
+import 'dart:collection';
+
+import 'package:logging/logging.dart';
+import 'package:meta/meta.dart';
+
+import 'package:w_common/src/disposable_vm/disposable_manager_vm.dart';
+
+// ignore: one_member_abstracts
+abstract class _Disposable {
+  Future<Null> dispose();
+}
+
+class _InternalDisposable implements _Disposable {
+  Disposer _disposer;
+
+  _InternalDisposable(this._disposer);
+
+  @override
+  Future<Null> dispose() {
+    var disposeFuture = _disposer != null ? _disposer() : null;
+    _disposer = null;
+    if (disposeFuture == null) {
+      return new Future.value();
+    }
+    return disposeFuture.then((_) => null);
+  }
+}
+
+/// A [Timer] implementation that exposes a [Future] that resolves when a
+/// non-periodic timer finishes it's callback or when any type of [Timer] is
+/// cancelled.
+class _ObservableTimer implements Timer {
+  Completer<Null> _didConclude = new Completer<Null>();
+  Timer _timer;
+
+  _ObservableTimer(Duration duration, void callback()) {
+    _timer = new Timer(duration, () {
+      callback();
+      _didConclude.complete();
+    });
+  }
+
+  _ObservableTimer.periodic(Duration duration, void callback(Timer t)) {
+    _timer = new Timer.periodic(duration, callback);
+  }
+
+  /// The timer has either been completed or has been cancelled.
+  Future<Null> get didConclude => _didConclude.future;
+
+  @override
+  void cancel() {
+    _timer.cancel();
+    if (!_didConclude.isCompleted) {
+      _didConclude.complete();
+    }
+  }
+
+  @override
+  bool get isActive => _timer.isActive;
+}
+
+/// A function that, when called, disposes of one or more objects.
+typedef Future<dynamic> Disposer();
+
+/// Allows the creation of managed objects, including helpers for common patterns.
+///
+/// There are four ways to consume this class: as a mixin, a base class,
+/// an interface, and a concrete class used as a proxy. All should work
+/// fine but the first is the simplest
+/// and most powerful. Using the class as an interface will require
+/// significant effort.
+///
+/// In the case below, the class is used as a mixin. This provides both
+/// default implementations and flexibility since it does not occupy
+/// a spot in the class hierarchy.
+///
+/// Helper methods, such as [manageStreamSubscription] allow certain
+/// cleanup to be automated. Managed subscriptions will be automatically
+/// canceled when [dispose] is called on the object.
+///
+///      class MyDisposable extends Object with Disposable {
+///        StreamController _controller = new StreamController();
+///
+///        MyDisposable(Stream someStream) {
+///          manageStreamSubscription(someStream.listen((_) => print('some stream')));
+///          manageStreamController(_controller);
+///        }
+///
+///        Future<Null> onDispose() {
+///          // Other cleanup
+///        }
+///      }
+///
+/// The [manageDisposer] helper allows you to clean up arbitrary objects
+/// on dispose so that you can avoid keeping track of them yourself. To
+/// use it, simply provide a callback that returns a [Future] of any
+/// kind. For example:
+///
+///      class MyDisposable extends Object with Disposable {
+///        StreamController _controller = new StreamController();
+///
+///        MyDisposable() {
+///          var thing = new ThingThatRequiresCleanup();
+///          manageDisposer(() {
+///            thing.cleanUp();
+///            return new Future(() {});
+///          });
+///        }
+///      }
+///
+/// Cleanup will then be automatically performed when the containing
+/// object is disposed. If returning a future is inconvenient or
+/// otherwise undesirable, you may also return `null` explicitly.
+///
+/// Implementing the [onDispose] method is entirely optional and is only
+/// necessary if there is cleanup required that is not covered by one of
+/// the helpers.
+///
+/// It is possible to schedule a callback to be called after the object
+/// is disposed for purposes of further, external, cleanup or bookkeeping
+/// (for example, you might want to remove any objects that are disposed
+/// from a cache). To do this, use the [didDispose] future:
+///
+///      var myDisposable = new MyDisposable();
+///      myDisposable.didDispose.then((_) {
+///        // External cleanup
+///      });
+///
+/// Below is an example of using the class as a concrete proxy.
+///
+///      class MyLifecycleThing implements DisposableManager {
+///        Disposable _disposable = new Disposable();
+///
+///        MyLifecycleThing() {
+///          _disposable.manageStreamSubscription(someStream.listen(() => null));
+///        }
+///
+///        @override
+///        void manageStreamSubscription(StreamSubscription sub) {
+///          _disposable.manageStreamSubscription(sub);
+///        }
+///
+///        // ...more methods
+///
+///        Future<Null> unload() async {
+///          await _disposable.dispose();
+///        }
+///      }
+///
+/// In this case, we want `MyLifecycleThing` to have its own lifecycle
+/// without explicit reference to [Disposable]. To do this, we use
+/// composition to include the [Disposable] machinery without changing
+/// the public interface of our class or polluting its lifecycle.
+class Disposable implements _Disposable, DisposableManagerV3 {
+  static bool _debugMode = false;
+  static Logger _logger;
+
+  /// Disables logging enabled by [enableDebugMode].
+  static void disableDebugMode() {
+    if (_debugMode) {
+      _debugMode = false;
+      _logger.clearListeners();
+      _logger = null;
+    }
+  }
+
+  /// Causes messages to be logged for various lifecycle and management events.
+  ///
+  /// This should only be used for debugging and profiling as it can result
+  /// in a huge number of messages being generated.
+  static void enableDebugMode() {
+    if (!_debugMode) {
+      _debugMode = true;
+      _logger = new Logger('Disposable');
+    }
+  }
+
+  final Set<Future> _awaitableFutures = new HashSet<Future>();
+  Completer<Null> _didDispose = new Completer<Null>();
+  final Set<_Disposable> _internalDisposables = new HashSet<_Disposable>();
+  bool _isDisposing = false;
+
+  /// A [Future] that will complete when this object has been disposed.
+  Future<Null> get didDispose => _didDispose.future;
+
+  /// The total size of the disposal tree rooted at the current Disposable
+  /// instance.
+  ///
+  /// This should only be used for debugging and profiling as it can incur
+  /// a significant performance penalty if the tree is large.
+  int get disposalTreeSize {
+    int size = 1;
+    for (var disposable in _internalDisposables) {
+      if (disposable is Disposable) {
+        size += disposable.disposalTreeSize;
+      } else {
+        size++;
+      }
+    }
+    return size;
+  }
+
+  /// Whether this object has been disposed.
+  bool get isDisposed => _didDispose.isCompleted;
+
+  /// Whether this object has been disposed or is disposing.
+  ///
+  /// This will become `true` as soon as the [dispose] method is called
+  /// and will remain `true` forever. This is intended as a convenience
+  /// and `object.isDisposedOrDisposing` will always be the same as
+  /// `object.isDisposed || object.isDisposing`.
+  bool get isDisposedOrDisposing => isDisposed || isDisposing;
+
+  /// Whether this object is in the process of being disposed.
+  ///
+  /// This will become `true` as soon as the [dispose] method is called
+  /// and will become `false` once the [didDispose] future completes.
+  bool get isDisposing => _isDisposing;
+
+  @mustCallSuper
+  @override
+  Future<T> awaitBeforeDispose<T>(Future<T> future) {
+    _throwOnInvalidCall('awaitBeforeDispose', 'future', future);
+    _awaitableFutures.add(future);
+    future.then((_) {
+      if (!isDisposedOrDisposing) {
+        _awaitableFutures.remove(future);
+      }
+    }).catchError((_) {
+      if (!isDisposedOrDisposing) {
+        _awaitableFutures.remove(future);
+      }
+    });
+    return future;
+  }
+
+  /// Dispose of the object, cleaning up to prevent memory leaks.
+  @override
+  Future<Null> dispose() async {
+    Stopwatch stopwatch;
+    if (_debugMode) {
+      stopwatch = new Stopwatch()..start();
+    }
+
+    _logDispose();
+
+    if (isDisposed) {
+      return null;
+    }
+    if (_isDisposing) {
+      return didDispose;
+    }
+    _isDisposing = true;
+
+    await Future.wait(_awaitableFutures);
+    _awaitableFutures.clear();
+
+    for (var disposable in _internalDisposables) {
+      await disposable.dispose();
+    }
+    _internalDisposables.clear();
+
+    await onDispose();
+
+    _completeDisposeFuture();
+
+    if (_debugMode) {
+      stopwatch.stop();
+      _logger.info(
+          '$runtimeType $hashCode took ${stopwatch.elapsedMicroseconds / 1000000.0} seconds to dispose');
+    }
+  }
+
+  @mustCallSuper
+  @override
+  Future<T> getManagedDelayedFuture<T>(Duration duration, T callback()) {
+    var completer = new Completer<T>();
+    var timer =
+        new _ObservableTimer(duration, () => completer.complete(callback()));
+    var disposable = new _InternalDisposable(() async {
+      timer.cancel();
+      completer.completeError(new ObjectDisposedException());
+    });
+    _logManageMessage(completer.future);
+    _internalDisposables.add(disposable);
+    timer.didConclude.then((Null _) {
+      if (!isDisposedOrDisposing) {
+        _logUnmanageMessage(completer.future);
+        _internalDisposables.remove(disposable);
+      }
+    });
+    return completer.future;
+  }
+
+  @mustCallSuper
+  @override
+  Timer getManagedTimer(Duration duration, void callback()) {
+    var timer = new _ObservableTimer(duration, callback);
+    _addObservableTimerDisposable(timer);
+    return timer;
+  }
+
+  @mustCallSuper
+  @override
+  Timer getManagedPeriodicTimer(Duration duration, void callback(Timer timer)) {
+    var timer = new _ObservableTimer.periodic(duration, callback);
+    _addObservableTimerDisposable(timer);
+    return timer;
+  }
+
+  @mustCallSuper
+  @override
+  Completer<T> manageCompleter<T>(Completer<T> completer) {
+    _throwOnInvalidCall('manageCompleter', 'completer', completer);
+    _logManageMessage(completer);
+
+    var disposable = new _InternalDisposable(() async {
+      if (!completer.isCompleted) {
+        completer.completeError(new ObjectDisposedException());
+      }
+    });
+    _internalDisposables.add(disposable);
+
+    completer.future.catchError((e) {
+      if (!isDisposedOrDisposing) {
+        _logUnmanageMessage(completer);
+        _internalDisposables.remove(disposable);
+      }
+    }).then((_) {
+      if (!isDisposedOrDisposing) {
+        _logUnmanageMessage(completer);
+        _internalDisposables.remove(disposable);
+      }
+    });
+
+    return completer;
+  }
+
+  @mustCallSuper
+  @override
+  void manageDisposable(Disposable disposable) {
+    _throwOnInvalidCall('manageDisposable', 'disposable', disposable);
+    _logManageMessage(disposable);
+
+    _internalDisposables.add(disposable);
+    disposable.didDispose.then((_) {
+      if (!isDisposedOrDisposing) {
+        _logUnmanageMessage(disposable);
+        _internalDisposables.remove(disposable);
+      }
+    });
+  }
+
+  @mustCallSuper
+  @override
+  void manageDisposer(Disposer disposer) {
+    _throwOnInvalidCall('manageDisposer', 'disposer', disposer);
+    _logManageMessage(disposer);
+
+    _internalDisposables.add(new _InternalDisposable(disposer));
+  }
+
+  @mustCallSuper
+  @override
+  void manageStreamController(StreamController controller) {
+    _throwOnInvalidCall('manageStreamController', 'controller', controller);
+    // If a single-subscription stream has a subscription and that
+    // subscription is subsequently canceled, the `done` future will
+    // complete, but there is no other way for us to tell that this
+    // is what has happened. If we then listen to the stream (since
+    // closing a stream that was never listened to never completes) we'll
+    // get an exception. This workaround allows us to "know" when a
+    // subscription has been canceled so we don't bother trying to
+    // listen to the stream before closing it.
+    _logManageMessage(controller);
+
+    bool isDone = false;
+
+    var disposable = new _InternalDisposable(() {
+      if (!controller.hasListener && !controller.isClosed && !isDone) {
+        controller.stream.listen((_) {});
+      }
+      return controller.close();
+    });
+
+    controller.done.then((_) {
+      isDone = true;
+      if (!isDisposedOrDisposing) {
+        _logUnmanageMessage(controller);
+        _internalDisposables.remove(disposable);
+      }
+      disposable.dispose();
+    });
+
+    _internalDisposables.add(disposable);
+  }
+
+  @mustCallSuper
+  @override
+  void manageStreamSubscription(StreamSubscription subscription) {
+    _throwOnInvalidCall(
+        'manageStreamSubscription', 'subscription', subscription);
+    _logManageMessage(subscription);
+
+    _internalDisposables
+        .add(new _InternalDisposable(() => subscription.cancel()));
+  }
+
+  /// Callback to allow arbitrary cleanup on dispose.
+  @protected
+  Future<Null> onDispose() async {
+    return null;
+  }
+
+  void _addObservableTimerDisposable(_ObservableTimer timer) {
+    _InternalDisposable disposable =
+        new _InternalDisposable(() async => timer.cancel());
+    _internalDisposables.add(disposable);
+    timer.didConclude.then((Null _) {
+      if (!isDisposedOrDisposing) {
+        _internalDisposables.remove(disposable);
+      }
+    });
+  }
+
+  void _completeDisposeFuture() {
+    _didDispose.complete();
+    _isDisposing = false;
+    if (_debugMode) {
+      _logger.info('Disposed object $hashCode of type $runtimeType');
+    }
+  }
+
+  void _logDispose() {
+    if (_debugMode) {
+      _logger.info('Disposing object $hashCode of type $runtimeType');
+    }
+  }
+
+  void _logUnmanageMessage(Object target) {
+    if (_debugMode) {
+      _logger.info(
+          '$runtimeType $hashCode unmanaging ${target.runtimeType} ${target.hashCode}');
+    }
+  }
+
+  void _logManageMessage(Object target) {
+    if (_debugMode) {
+      _logger.info(
+          '$runtimeType $hashCode managing ${target.runtimeType} ${target.hashCode}');
+    }
+  }
+
+  void _throwOnInvalidCall(
+      String methodName, String parameterName, dynamic parameterValue) {
+    if (parameterValue == null) {
+      throw new ArgumentError.notNull(parameterName);
+    }
+    if (isDisposing) {
+      throw new StateError('$methodName not allowed, object is disposing');
+    }
+    if (isDisposed) {
+      throw new StateError(
+          '$methodName not allowed, object is already disposed');
+    }
+  }
+}
