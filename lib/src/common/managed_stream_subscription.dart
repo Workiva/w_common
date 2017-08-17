26d2f06e744c92014fd83059222f456eb7423f86
George Lesica <george.lesica@workiva.com>
2017-08-16 20:25:58 -0600

    Complete subscription future on done or error

--- lib/src/common/managed_stream_subscription.dart
+++ lib/src/common/managed_stream_subscription.dart
@@ -5,16 +5,31 @@ import 'dart:async';
 /// The [didCancel] future is used to provide an anchor point for removing
 /// internal references to `StreamSubscriptions` if consumers manually cancel the
 /// subscription. This class is not publicly exported.
+///
+/// There are three situations in which [didCancel] will be completed
 class ManagedStreamSubscription<T> implements StreamSubscription<T> {
+  final bool _cancelOnError;
+
   final StreamSubscription<T> _subscription;
 
   Completer<Null> _didCancel = new Completer();
 
   ManagedStreamSubscription(Stream<T> stream, void onData(T),
-      {Function onError, void onDone(), bool cancelOnError})
-      : _subscription = stream.listen(onData,
-            onError: onError, onDone: onDone, cancelOnError: cancelOnError);
+      {void onError(error, [stackTrace]),
+      void onDone(),
+      bool cancelOnError: false})
+      : _cancelOnError = cancelOnError,
+        _subscription = stream.listen(onData, cancelOnError: cancelOnError) {
+    _wrapOnDone(onDone);
+    _wrapOnError(onError);
+  }
 
+  Future<Null> get didCancel => _didCancel.future;
+
+  @override
+  bool get isPaused => _subscription.isPaused;
+
+  // TODO: Should we complete when the resulting future completes?
   @override
   Future<E> asFuture<E>([E futureValue]) => _subscription.asFuture(futureValue);
 
@@ -26,36 +41,62 @@ class ManagedStreamSubscription<T> implements StreamSubscription<T> {
     // This behavior is described in the docs as "for historical reasons" so
     // this may change in the future.
     if (result == null) {
-      if (!_didCancel.isCompleted) {
-        _didCancel.complete();
-      }
+      _complete();
       return null;
     }
 
     return result.then((_) {
-      if (!_didCancel.isCompleted) {
-        _didCancel.complete();
-      }
+      _complete();
     });
   }
-
-  Future<Null> get didCancel => _didCancel.future;
-
-  @override
-  bool get isPaused => _subscription.isPaused;
 
   @override
   void onData(void handleData(T _)) => _subscription.onData(handleData);
 
   @override
-  void onDone(void handleDone()) => _subscription.onDone(handleDone);
+  void onDone(void handleDone()) => _wrapOnDone(handleDone);
 
   @override
-  void onError(Function handleError) => _subscription.onError(handleError);
+  void onError(Function handleError) => _wrapOnError(handleError);
 
   @override
   void pause([Future resumeSignal]) => _subscription.pause(resumeSignal);
 
   @override
   void resume() => _subscription.resume();
+
+  void _complete() {
+    if (!_didCancel.isCompleted) {
+      _didCancel.complete();
+    }
+  }
+
+  void _wrapOnDone(void handleDone()) {
+    _subscription.onDone(() {
+      if (handleDone != null) {
+        handleDone();
+      }
+
+      _complete();
+    });
+  }
+
+  void _wrapOnError(void handleError(error, [stackTrace])) {
+    _subscription.onError((error, [stackTrace]) {
+      if (handleError == null) {
+        // By default unhandled stream errors are handled by their zone
+        // error handler. In this case we *always* handle errors,
+        // but the consumer may actually want the default behavior,
+        // so in the case where the handler given to us by the consumer
+        // is null (which is the default) we take the default action.
+        Zone.current.handleUncaughtError(error, stackTrace);
+      } else {
+        handleError(error, stackTrace);
+      }
+
+      if (_cancelOnError) {
+        _complete();
+      }
+    });
+  }
 }
