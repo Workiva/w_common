4105830067dd7241d5b4e7018b030e485f97c92a
George Lesica <george.lesica@workiva.com>
2018-08-03 09:00:31 -0600

    Remove dart_tasks

--- tool/test.sh
+++ tool/test.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Fast fail the script on failures.
 set -e
+
 DART_VERSION=$(dart --version 2>&1)
 DART_2_PREFIX="Dart VM version: 2"
+
 if [[ $DART_VERSION = $DART_2_PREFIX* ]]; then
     echo -e 'pub run build_runner test -- -p chrome -p vm --reporter=expanded'
     pub run build_runner test -- -p chrome -p vm --reporter=expanded
 else
     echo -e 'pub publish --dry-run'
     pub publish --dry-run # Validate package
+
     echo -e 'pub run dart_dev gen-test-runner --check'
     pub run dart_dev gen-test-runner --check
+
     echo -e 'pub run dart_dev test --pub-serve --web-compiler=dartdevc -p chrome -p vm'
     pub run dart_dev test --pub-serve --web-compiler=dartdevc -p chrome -p vm
 fi 
