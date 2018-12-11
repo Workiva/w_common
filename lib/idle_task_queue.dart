af9f31a11a69798e8c85412da27efc2c0aeb8ea6
Todd Beckman <todd.beckman@workiva.com>
2018-12-11 14:47:39 -0700

    handle tasks throwing

--- lib/idle_task_queue.dart
+++ lib/idle_task_queue.dart
@@ -7,6 +7,8 @@ import 'dart:js';
 
 import 'package:meta/meta.dart';
 
+const Duration _immediately = const Duration();
+
 /// A handler which will be called during idle time.
 typedef void IdleTaskHandler<T>(T input);
 
@@ -14,20 +16,27 @@ typedef void IdleTaskHandler<T>(T input);
 class _IdleTask<T> {
   final IdleTaskHandler<T> handler;
   final T input;
+  final Completer<Null> completer = new Completer<Null>();
 
   _IdleTask(this.handler, this.input);
 }
 
 /// Adds the task to a queue which will be called during idle time.
-void enqueueIdleTask<T>(IdleTaskHandler<T> handler, T input) {
+///
+/// Returns the `Future` which will complete when the task has run.
+///
+/// May complete with an error if the task throws when called.
+Future<Null> enqueueIdleTask<T>(IdleTaskHandler<T> handler, T input) {
   // For unsupported browsers when the polyfill is not available,
   // we still want the task to be called eventually.
   if (!_doesBrowserSupportIdleCallback) {
-    new Future.delayed(const Duration()).then((_) => handler(input));
-    return;
+    return new Future.delayed(_immediately).then((_) async {
+      handler(input);
+    });
   }
 
-  _taskQueue.add(new _IdleTask<T>(handler, input));
+  final task = new _IdleTask<T>(handler, input);
+  _taskQueue.add(task);
 
   // Start running the queue if it has not been started.
   // If there is a task, the queue is already running and doesn't
@@ -38,6 +47,8 @@ void enqueueIdleTask<T>(IdleTaskHandler<T> handler, T input) {
       <String, dynamic>{'timeout': 1000},
     );
   }
+
+  return task.completer.future;
 }
 
 void _runQueue(IdleDeadline deadline) {
@@ -45,7 +56,17 @@ void _runQueue(IdleDeadline deadline) {
       deadline.timeRemaining() > 0 &&
       _taskQueue.isNotEmpty) {
     final task = _taskQueue.removeAt(0);
-    task.handler(task.input);
+    try {
+      task.handler(task.input);
+      task.completer.complete();
+    } catch (e, t) {
+      // Listeners to the completer future might throw.
+      if (!task.completer.isCompleted) {
+        try {
+          task.completer.completeError(e, t);
+        } catch (_) {}
+      }
+    }
   }
 
   if (_taskQueue.isNotEmpty) {
