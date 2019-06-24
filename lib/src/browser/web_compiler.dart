c7626e7592a6b780606e291d5afaf5f6071ecef5
Smai Fullerton <smai.fullerton@workiva.com>
2019-06-24 13:57:46 -0600

    Use @visibleForTesting; improve comment

--- lib/src/browser/web_compiler.dart
+++ lib/src/browser/web_compiler.dart
@@ -2,15 +2,19 @@
 library web_compiler;
 
 import 'package:js/js.dart';
+import 'package:meta/meta.dart';
 
 @JS(r'$dartLoader')
 external dynamic get _dartLoader;
 
-/// The compiler used to compile the Dart to JS, determined at runtime
-/// via a global window property that is only present when DDC is used.
+/// Whether or not the Dart dev compiler (DDC), as opposed to the
+/// production compiler (dart2js), was used to compile the app.
 ///
-/// If false, dart2js was used. Works with Dart 2 only.
+/// It is determined at runtime via a global window property that
+/// is only present when DDC is used. It works with Dart 2 only.
+///
 /// Per the Dart team, the presence of this window property isn't
 /// guaranteed forever, so proceed with caution and only use if you
 /// absolutely must to work around compiler bugs.
+@visibleForTesting
 bool get isCompiledWithDdc => _dartLoader != null;
