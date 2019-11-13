2782545a329542c784f50c827aa437851066fe91
olesiathoms-wk <olesia.thoms@workiva.com>
2019-11-13 12:54:19 -0700

    specify platforms for test

--- test/unit/vm/compile_sass_test.dart
+++ test/unit/vm/compile_sass_test.dart
@@ -1,4 +1,3 @@
-@TestOn('vm')
 // Copyright 2019 Workiva Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+@TestOn('vm')
 import 'dart:io';
 
 import 'package:glob/glob.dart';
