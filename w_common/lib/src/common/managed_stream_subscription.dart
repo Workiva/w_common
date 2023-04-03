2de45eab2dd368c04572a77462140c144f9cb492
Tim Gilbert <tim.gilbert@workiva.com>
2023-04-03 16:10:52 -0500

    Update managed_stream_subscription.dart

--- w_common/lib/src/common/managed_stream_subscription.dart
+++ w_common/lib/src/common/managed_stream_subscription.dart
@@ -40,6 +40,14 @@ class ManagedStreamSubscription<T> implements StreamSubscription<T> {
   Future<void> cancel() {
     var result = _subscription.cancel();
 
+    // StreamSubscription.cancel() will return null if no cleanup was
+    // necessary. This behavior is described in the docs as "for historical
+    // reasons" so this may change in the future.
+    if (result == null) {
+      _complete();
+      return Future(() {});
+    }
+
     return result.then((_) {
       _complete();
     });
