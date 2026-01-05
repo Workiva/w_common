bc61969bc90de3a5a0137ac675c3d0c66be92d6b
Abe Scheideman <abe.scheideman@workiva.com>
2026-01-05 15:51:27 -0600

    format

--- w_common/lib/src/intl/time_intl.dart
+++ w_common/lib/src/intl/time_intl.dart
@@ -3,5 +3,6 @@ import 'package:intl/intl.dart';
 class TimeIntl {
   static String get today => Intl.message('Today', name: 'TimeIntl_today');
 
-  static String get yesterday => Intl.message('Yesterday', name: 'TimeIntl_yesterday');
+  static String get yesterday =>
+      Intl.message('Yesterday', name: 'TimeIntl_yesterday');
 }
